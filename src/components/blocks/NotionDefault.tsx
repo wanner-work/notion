f18c9f99844597e6943b6154dfb6ed2495060d81
jwanner83 <jonas@wanner.com>
2024-05-09 19:44:09 +0800

    feat: improved the default component and changed the link

--- src/components/blocks/NotionDefault.tsx
+++ src/components/blocks/NotionDefault.tsx
@@ -2,15 +2,15 @@ import NotionBlockObject from '../../interfaces/NotionBlockObject'
 
 export default function NotionDefault({ block }: NotionBlockObject) {
   return (
-    <div className="bg-red-600/20 p-5 mt-5 text-red-500 font-mono rounded text-sm">
-      <p className="font-bold mb-2">Unsuported type</p>
-      <p className="mb-2">
+    <div className="bg-amber-300/20 p-5 mt-5 text-amber-300 font-mono rounded">
+      <p className="font-bold mb-3">Unsuported type</p>
+      <p className="mb-1 text-sm">
         The block type <span className="p-1 bg-black/40 text-white/70 text-[12px] rounded">{block.type}</span> is currently not supported by <span
         className="p-1 bg-black/40 text-white/70 text-[12px] rounded">@wanner.work/notion</span>. But you can still render it by creating a custom
         component.
       </p>
-      <p>
-        See <a className="underline" href="">https://github.com/wanner-work/notion/</a> for more information on how to
+      <p className="text-sm">
+        See <a className="underline" href="https://github.com/wanner-work/notion?tab=readme-ov-file#customizing-the-rendering-of-the-blocks" target="_blank">https://github.com/wanner-work/notion?tab=readme-ov-file#customizing-the-rendering-of-the-blocks</a> for more information on how to
         create and integrate custom components.
       </p>
     </div>
