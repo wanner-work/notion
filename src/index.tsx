9066260d45c5586d41637443f4caea58ff56ff1d
jwanner83 <jonas@wanner.com>
2024-05-28 16:01:31 +0930

    feat: releasing 1.2.0

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import NotionQuery from './classes/NotionQuery'
 import Notion from './components/Notion'
 import NotionBlock from './components/NotionBlock'
 import NotionBlockObject from './interfaces/NotionBlockObject'
+import NotionBlockTypes from './interfaces/NotionBlockTypes'
+import NotionCustomBlock from './interfaces/NotionCustomBlock'
 
 export { NotionBlock, NotionQuery }
 
-export type { NotionBlockObject }
+export type { NotionBlockObject, NotionBlockTypes, NotionCustomBlock }
 
 export default Notion
