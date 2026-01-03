c39e4fb90415d6743db64a575ddfac6f70e8eaf0
Jonas Wanner <jonas.wanner@clavisit.com>
2026-01-03 12:47:06 +0100

    docs: fix dependency to notion package

--- modules/docs/src/interfaces/temp/NotionQuery.ts
+++ modules/docs/src/interfaces/temp/NotionQuery.ts
@@ -3,11 +3,74 @@ import type {
   DataSourceObjectResponse,
   DatabaseObjectResponse,
   GetDatabaseParameters,
-  ListBlockChildrenResponse,
   PageObjectResponse,
   QueryDataSourceParameters
 } from '@notionhq/client/build/src/api-endpoints'
-import type { NotionQueryData, NotionBlockObject } from '@wanner.work/notion'
+import {BlockObjectResponse} from "@notionhq/client";
+import {FunctionComponent} from "react";
+
+type NotionBlockTypes = BlockObjectResponse['type'];
+
+interface NotionCustomBlock {
+  /**
+   * The type of the Notion block.
+   */
+  type: NotionBlockTypes;
+  /**
+   * The React component used to render the Notion block.
+   */
+  component: FunctionComponent<NotionBlockObject<any>>;
+}
+
+interface NotionRenderConfig {
+  /**
+   * Custom blocks to override default Notion block rendering.
+   * Each custom block should specify the `type` of Notion block it overrides
+   * and the corresponding React component to render that block.
+   *
+   * @example
+   * ```ts
+   * import NotionCalloutBlock from "@/components/blocks/NotionCalloutBlock";
+   *
+   * const config: NotionRenderConfig = {
+   *   blocks: [
+   *     {
+   *       type: "callout",
+   *       component: NotionCalloutBlock
+   *     }
+   *   ]
+   * }
+   * ```
+   */
+  blocks?: NotionCustomBlock[];
+  /**
+   * If set to true, suppresses warnings for unsupported Notion block types.
+   * It is recommended to set this to true in production environments.
+   */
+  hideUnsupportedBlockWarning?: boolean;
+}
+
+interface NotionBlockObject<BlockTypeObjectResponse = BlockObjectResponse> {
+  /**
+   * The Notion block object.
+   */
+  block: BlockTypeObjectResponse;
+  /**
+   * An array of child Notion block objects, if any.
+   */
+  sub?: NotionBlockObject[];
+  /**
+   * The nesting level of the Notion block within the document structure.
+   * Defaults to 0 for top-level blocks.
+   */
+  level?: number;
+  /**
+   * Configuration options for rendering Notion blocks.
+   */
+  config?: NotionRenderConfig;
+}
+
+type NotionQueryData = NotionBlockObject[];
 
 export interface NotionQuery {
   /**
